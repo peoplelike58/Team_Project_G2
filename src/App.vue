ad2fd1b501fe0afc59200c7fc4cafcd7e852c269
Irene-LIN618 <irene8study@gmail.com>
2025-08-16 17:15:13 +0800

    fix date

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-</script> -->
+</script>
 
 <!-- <template>
   <header>
@@ -83,3 +83,4 @@ nav a:first-of-type {
   }
 }
 </style>
+-->
--- src/main.js
+++ src/main.js
@@ -1,25 +1,19 @@
-<<<<<<< HEAD
 import '@/assets/styles/main.scss'
 import 'element-plus/dist/index.css'//element-plus
 import ElementPlus from 'element-plus'
 
-=======
->>>>>>> dev
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-<<<<<<< HEAD
-=======
 // Element Plus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 //全域 SCSS
 import '@/assets/styles/main.scss'
->>>>>>> dev
 
 const app = createApp(App)
 
