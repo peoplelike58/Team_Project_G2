caf786807636d346e666c8316e3108b8186e0ae8
summerSnow <lovepeace.yuki@gmail.com>
2025-08-26 12:05:31 +0800

    [feat]檔案路徑修改

---
+++ src/pages/PeakGuide.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import NavMenu from '@/components/An/navMenu.vue';
+    import PeakIntro from '@/pages/Peak/PeakIntro.vue';
+    import PeakMap from '@/pages/Peak/PeakMap.vue';
+</script>
+
+
+<template>
+    <NavMenu/>
+    <PeakIntro />
+    <PeakMap />
+</template>
