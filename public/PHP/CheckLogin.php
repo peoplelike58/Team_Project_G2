5031406d6f9b5c3da5667a1a815a6c0a7fc0183d
Irene-LIN618 <irene8study@gmail.com>
2025-09-04 15:38:54 +0800

    fix [php inclucde + ;]

--- public/PHP/CheckLogin.php
+++ public/PHP/CheckLogin.php
@@ -1,7 +1,7 @@
 <?php
 $member=json_decode(file_get_contents("php://input"), true);//接收前端來的東西，做json檔的解碼，file_get_contents("php://input") → 抓到整個 JSON 字串，json_decode(..., true) → 把 JSON 轉成 關聯陣列 (associative array)
 
-include 'toMysql.php'
+include 'toMysql.php';
 
 
 session_start();
--- public/PHP/LoginPage_register.php
+++ public/PHP/LoginPage_register.php
@@ -3,7 +3,7 @@
 $member=json_decode(file_get_contents("php://input"), true);//接收前端來的東西，做json檔的解碼，file_get_contents("php://input") → 抓到整個 JSON 字串，json_decode(..., true) → 把 JSON 轉成 關聯陣列 (associative array)
 
 
-include 'toMysql.php'
+include 'toMysql.php';
 
 
 $statement = $pdo->prepare("select EMAIL from MEMBER where EMAIL = :email");
