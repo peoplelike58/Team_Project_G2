a3bad24926ce5b256116617649e9199c292e62b5
summerSnow <lovepeace.yuki@gmail.com>
2025-09-08 12:00:48 +0800

    [feat] 串接資料庫

--- src/pages/admin/NewsPage.vue
+++ src/pages/admin/NewsPage.vue
@@ -3,16 +3,32 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from "vue";
 import CrudPage from '@/components/common/CrudPage.vue'
 const columns = [
-  { prop: 'postId', label: '文章編號' },
-  { prop: 'publishDate', label: '上架日期' ,type:"date"},
-  { prop: 'category', label: '分類' },
-  { prop: 'title', label: '標題' },
-  { prop: 'status', label: '上下架', type:'select', options:[{label:'上架',value:'上架'},{label:'下架',value:'下架'}] },
-  { prop: 'updatedAt', label: '最後更新日期' ,type:"date"}
+  { prop: 'NEWS_ID', label: '文章編號' },
+  { prop: 'UPLOAD_AT', label: '上架日期' ,type:"date"},
+  { prop: 'TYPE', label: '分類' },
+  { prop: 'TITLE', label: '標題' },
+  { prop: 'STATUS', label: '上下架', type:'select', options:[{label:'上架',value:'上架'},{label:'下架',value:'下架'}] },
+  { prop: 'UPDATED_AT', label: '最後更新日期' ,type:"date"}
 ]
-const sampleData = [
-  { postId: 'N-001', publishDate: '2025-07-10', category: '安全小知識', title: '夏季補水要點', status: '上架', updatedAt: '2025-07-12' }
-]
+// const sampleData = [
+//   { postId: 'N-001', publishDate: '2025-07-10', category: '安全小知識', title: '夏季補水要點', status: '上架', updatedAt: '2025-07-12' }
+// ]
+
+  const sampleData =ref([])
+  
+  onMounted(()=>{
+    fetch('http://localhost/Mountain_Peak/NewsPage.php')
+      .then(resp => resp.json())
+      .then(json =>{
+        sampleData.value = json
+        // console.log(sampleData);
+      }) 
+
+  })
+
+
+
 </script>
--- src/pages/admin/ProductPage.vue
+++ src/pages/admin/ProductPage.vue
@@ -3,17 +3,33 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from "vue";
 import CrudPage from '@/components/common/CrudPage.vue'
 const columns = [
-  { prop: 'sku', label: '商品編號' },
-  { prop: 'category', label: '商品類別' },
-  { prop: 'name', label: '名稱' },
-  { prop: 'price', label: '售價' },
+  { prop: 'PRODUCT_ID', label: '商品編號' },
+  { prop: 'PRODUCT_TYPE', label: '商品類別' },
+  { prop: 'PRODUCT_NAME', label: '名稱' },
+  { prop: 'PRICE', label: '售價' },
   { prop: 'stock', label: '庫存量' },
-  { prop: 'status', label: '上下架', type: 'select', options:[{label:'上架',value:'上架'},{label:'下架',value:'下架'}] }
+  { prop: 'PRODUCT_STATUS', label: '上下架', type: 'select', options:[{label:'上架',value:'上架'},{label:'下架',value:'下架'}] }
 ]
-const sampleData = [
-  { sku: 'GEAR-001', category: '背包', name: '輕量登山包 30L', price: 2680, stock: 42, status: '上架' },
-  { sku: 'GEAR-002', category: '登山杖', name: '碳纖維登山杖', price: 1890, stock: 20, status: '上架' }
-]
+// const sampleData = [
+//   { sku: 'GEAR-001', category: '背包', name: '輕量登山包 30L', price: 2680, stock: 42, status: '上架' },
+//   { sku: 'GEAR-002', category: '登山杖', name: '碳纖維登山杖', price: 1890, stock: 20, status: '上架' }
+// ]
+
+const sampleData =ref([])
+  
+  onMounted(()=>{
+    fetch('http://localhost/Mountain_Peak/ProductPage.php')
+      .then(resp => resp.json())
+      .then(json =>{
+        sampleData.value = json
+        // console.log(sampleData);
+      }) 
+
+  })
+
+
+
 </script>
