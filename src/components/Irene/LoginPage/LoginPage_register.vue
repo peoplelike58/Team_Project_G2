64812cb957247f62390455ca8bb1ce83baee57d0
summerSnow <lovepeace.yuki@gmail.com>
2025-09-08 12:03:00 +0800

    [feat] modify type from submit to button

--- src/components/Irene/LoginPage/LoginPage_register.vue
+++ src/components/Irene/LoginPage/LoginPage_register.vue
@@ -102,7 +102,7 @@
 
       
       <!-- 立即註冊按鈕 -->
-      <button type="submit" class="submit-btn" :disabled="!isFormValid" @click="GoRegister">
+      <button type="button" class="submit-btn" :disabled="!isFormValid" @click="GoRegister">
         <!-- 暫時沒有認證，直接註冊成功 -->
         立即註冊
       </button>
@@ -232,9 +232,13 @@ const GoRegister = () => {
     alert("請先完成驗證！")
     return;
   }
-  
-  fetch('http://localhost/Mountain_Peak/LoginPage_register.php',{
-  // fetch('/tjd102/g2/PHP/LoginPage_register.php', {   //http://localhost/teamproject/LoginPage_register.php（local端測試網址）
+
+  //YUKI本機端測試
+  // fetch('http://localhost/Mountain_Peak/LoginPage_register.php',{   
+  //＝================================================================
+
+
+  fetch('/tjd102/g2/PHP/LoginPage_register.php', {   //http://localhost/teamproject/LoginPage_register.php（local端測試網址）
   method: 'POST',
   headers:{'Content-Type':'application/json'},
   credentials: 'include',
