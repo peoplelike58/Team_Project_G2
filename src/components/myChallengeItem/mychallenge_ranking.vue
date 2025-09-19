da489dc0c09ef8d9cac9efabe3bb8a4d2434ef32
ziwi9561 <chenyi35736@gmail.com>
2025-09-20 03:09:10 +0800

    debug 新增忘記密碼部屬後無法使用的除錯

--- src/components/myChallengeItem/mychallenge_ranking.vue
+++ src/components/myChallengeItem/mychallenge_ranking.vue
@@ -2,7 +2,10 @@
     <section v-for="(rank, index) in ranks" :key="index" class="nomb">
         <div class="personInfo" @click="toggle(index)">
             <div class="personInfoTitle">
-                <img :src="`${BASE}uploads/avatars/${rank.image}`" alt="" class="head">
+                <img :src="getAvatarPath(rank.image)"
+                    alt="頭像" 
+                    class="head"
+                    @error="handleImageError">
                 <h4 class="place">{{ rankText(index) }}<br />{{ rankIcon(index) }}</h4>
                 <h4>{{ rank.name }}</h4>
             </div>
@@ -60,6 +63,14 @@ import axios from 'axios'
         }else{
             openItem.value = index
         }
+    }
+
+    const getAvatarPath = (image) => {
+        return `${BASE}uploads/avatars/${image || 'default-avatar.png'}`
+    }
+
+    const handleImageError = (event) => {
+        event.target.src = `${BASE}uploads/avatars/default-avatar.png`
     }
 
     const ranks = ref([])
